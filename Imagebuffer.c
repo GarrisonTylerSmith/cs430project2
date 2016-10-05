abe282a6d76e27f3febb882c3a17c3fad365c363
Garrison Smith <Garrison Smith>
2016-10-04 18:00:58 -0700

    Initial commit

---
+++ Imagebuffer.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	unsigned char red,green,blue;
+}
+PPMPixel;
+typedef struct{
+	unsigned char red,green,blue;
+	int x, y;
+	PPMPixel *data;
+}
+PPMImage;
+typedef struct{
+	int width;
+	int height;
+	int max;
+	int type;
+}PPMmeta;
+#define CREATOR "GarrisonSmith"
+#define RGB_COMPONENT_COLOR 255
+
+static PPMImage *readPPM(const char *filename){
+	printf(filename);
+	char buff[16];
+	PPMImage *img;
+	FILE *fp;
+	int c, rgb_comp_color;
+
+	// this is where we would open the file for reading
+	// the PPM file to be exact
+	//printf("readppm %s\n", filename);
+	fp = fopen(filename, "rb");
+	if(!fp){
+		fprintf(stderr, "Unable to open file '%s'\n", filename);
+		exit(1);
+	}
+	// now we will read the format of the image
+	if(!fgets(buff, sizeof(buff), fp)){
+		perror(filename);
+		exit(1);
+	} 
+
+// check the format of the image
+	if(buff[0] != 'P' && (buff[1] != '6' || buff[1] != '3')){
+		fprintf(stderr, "Invalid image format (MUST BE P6 or P3)'%c'\n", buff[0]);
+		exit(1);
+	}
+	// alloc memory from image
+	img = (PPMImage *)malloc(sizeof(PPMImage));
+	if(!img){
+		fprintf(stderr, "Unable to allocate memory '%s'\n" );
+		exit(1);
+
+	}
+	// next we need to check for comments
+	c = getc(fp);
+	while(c == '#'){
+	while(getc(fp) != '\n');
+		c = getc(fp);
+	}
+	ungetc(c,fp);
+	// here we will read the image size and information about it
+	if(fscanf(fp, "%d %d", &img->x, &img->y) != 2){
+		fprintf(stderr, "Invalid image size (error loading '%s')\n", filename);
+		exit(1);
+	}
+	// read rgb component
+	if (fscanf(fp, "%d", &rgb_comp_color)!= 1){
+		fprintf(stderr, "Invalid rgb component(error Loading '%s')\n", filename);
+		exit(1);
+	}
+	// check rgb component depth 
+	if(rgb_comp_color != RGB_COMPONENT_COLOR){
+		fprintf(stderr, "'%s'does not have 8-bits components\n", filename);
+		exit(1);
+	}
+	while(fgetc(fp) != '\n');
+	// memory allocation for pixel data
+	img->data = (PPMPixel*)malloc(img->x * img->y * sizeof(PPMPixel));
+
+	if(!img){
+		fprintf(stderr, "Unable to allocate memory\n" );
+		exit(1);
+	}
+
+	//read pixel data from file
+	if(fread(img->data, 3, img->x * img->y, fp) != img->x * img->y){
+		fprintf(stderr, "Error loading image '%s'\n", filename);
+		exit(1);
+	}
+	fclose(fp);
+	return img;
+}
+void writePPM(const char *filename, PPMImage *img, PPMmeta meta){
+	FILE *fp;
+	int type;
+	
+	// open file for output
+	fp = fopen(filename, "wb");
+	if(!fp){
+		fprintf(stderr, "Unable to open file '%s'\n", filename);
+		exit(1);
+	}
+	// write the header file
+	// image format
+	fprintf(fp, "P%d\n", type);
+	// comments
+	fprintf(fp, "# Created by %s\n", CREATOR);
+	// image size
+	fprintf(fp, "%d %d\n", img->x,img->y);
+	// rgb component depth
+	fprintf(fp, "%d", RGB_COMPONENT_COLOR);
+	//fprintf(fp, "%s", img->data);
+
+	// pixel data
+	if(type == 3){
+		//fprintf(fp, "%s", img->data);
+		for(int i=0;i<img->x*img->y;i++) {
+			fprintf(fp, "%d\n", img->data[i].red);
+			fprintf(fp, "%d\n", img->data[i].green);
+			fprintf(fp, "%d\n", img->data[i].blue);
+		}
+	}
+	else {
+		fwrite(img->data, 3, img->y * img->x, fp);
+	}
+	fclose(fp);
+}
+void changeColorPPM(PPMImage *img){
+	if(img){
+		for(int i=0;i<img->x*img->y;i++){
+			img->data[i].red=RGB_COMPONENT_COLOR-img->data[i].red;
+			img->data[i].green=RGB_COMPONENT_COLOR-img->data[i].green;
+			img->data[i].blue=RGB_COMPONENT_COLOR-img->data[i].blue;
+
+		}
+	}
+}
+// int main(int argc, char *argv[]){
+// 	printf("starting..\n");
+//  	char* target = argv[1];
+//  	char* input = argv[2];
+//  	char* output = argv[3];
+//  	printf("target='%s' input='%s' output='%s'\n", target, input, output);
+//  	PPMImage *image;
+// 	image = readPPM(input);
+//  	//changeColorPPM(image);
+//  	writePPM(output, image, atoi(target));
+//  	return 0;
+//  }
---
+++ ray_caster.c
@@ -0,0 +1,272 @@
+// polymorphism in c
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "Imagebuffer.c"
+#include "Parsingjson.c"
+
+# define T_SPHERE 1
+# define T_PLANE 2
+# define T_CYLINDER 3
+# define T_CAMERA 4
+typedef struct{
+	int kind; // 0 = cylinder, 1 = sphere, 2 = plane
+	double color[3];
+	double a;
+	double b;
+	double c;
+	double d;
+	union{
+		struct {
+			double center[3];
+			double radius;
+			double height;
+		}cylinder;
+		struct{
+			double center[3];
+			double radius;
+		}sphere;
+		struct{
+			double position;
+			double normal;
+		}plane;
+		struct{
+			double position;
+			double width;
+			double height;
+		}camera;
+	};
+}Object;
+typedef struct{
+	int num_objects;
+	Object objects[128];
+	double camera_width;
+	double camera_height;
+	double camera_position[3];
+	double camera_facing[3];
+	double background_color[3];
+}Scene;
+
+static inline double sqr(double v){
+	return v*v;
+}
+static inline void normalize(double* v){
+	double len = sqrt(sqr(v[0]) + sqr(v[1]) + sqr(v[2]));
+	v[0] /= len;
+	v[1] /= len;
+	v[2] /= len;
+}
+double dot(double v[], double u[], int n){
+	double result = 0;
+	for(int i=0; i < n; i++){
+		result += v[i]*u[i];
+	}
+	return result;
+}
+
+double cylinder_intersecion(double* Ro, double* Rd, double* c, double r){
+	// Step 1 . Find the equation for the object 
+	// we are interested in (this case a cylinder)
+	// this is essential for the camera view point
+	// x^2 +z^2 = r^2
+	// Step 2. Parameterize the equation with a center point (if needeed)
+	// (x-Cx)^2 + (z-Cz)^2 = r^2
+	// step 3. Substitute the eq for a ray into our object eq.
+	//
+	//(RoX + RDX*t - Cx)^2 + ( Roz + t*Rdz - Cz)^2 - r^2 = 0
+	//
+	// Step 4. Solve for t .
+	//
+	//Step 4.a Rewrite the equation(flatten).
+	//
+	// -r^2 + t^2 * Rdx^2 + t^2 * Rdz^2 + 2*t * Rox * Rdx - 2*t * Rdx * Cx + 2*t * Roz * Rdz - 2*t * Rdz *cz + Rox^2 - 2*Rox*cx + cx^2 + Roz^2 - 2* Roz* Cz + Cz^2 = 0
+	//
+	// Step 4.b Rewrite the equation in terms of t.
+	//
+	// t^2 * (Rdx^2 + Rdz^2) + 
+	// t* (2 * (Rox * Rdx - Rdx * cx + Roz * Rdz - Rdz * Cz)) + 
+	// Rox^2 - 2*Rox*cx + cx^2 + Roz^2 - 2* Roz* Cz + Cz^2 - r^2 = 0
+	// Use quadratic equation to solvew for t..
+	//
+	double a = (sqr(Rd[0]) + sqr(Rd[2]));
+	double b = (2 * (Ro[0] * Rd[0] - Rd[0] * c[0] + Ro[2] * Rd[2] - Rd[2] * c[2]));
+	double d = (sqr(Ro[0]) - 2*Ro[0]*c[0] + sqr(c[0]) + sqr(Ro[2]) - 2* Ro[2]*c[2] + sqr(c[2]) - sqr(r));
+	double det = sqr(b) - 4*a*d; 
+	if( det < 0 ) return -1;
+
+	det = sqrt(det);
+
+	double t0 = (-b - det) / (2*a);
+	if (t0 > 0 ) return t0;
+
+	double t1 = (-b + det) / (2*a);
+	if(t1 > 0) return t1;
+
+	return -1;
+
+}
+double ray_plane_intersection(double a, double b, double c, double d, double* r0, double* rd){
+	return (a*r0[0] + b*r0[1] + c*r0[2] + d) / (a*rd[0] + b*rd[1] + c*rd[2]);
+	//Ray: R(t) = Ro + Rd*t
+	//Plane: ax + by + cz + d = 0
+	//solve for t
+	//n is the normal plane
+	// D is the distance from the origin
+	// do subsituting we get 
+	// t = -(n*Ro + d)/ (n * Rd) 
+
+	// double a = -(n*Ro + d)
+	// double b = (n*Rd)
+	// double det = (a/b)
+	// if(det < 0) return -1;
+}
+double ray_sphere_intersection(double* c, double r, double* Ro, double* Rd){
+	// Ray: P = Ro + Rd*t
+	// Sphere: (x-xc)^2 + (y-yc)^2 + (z-zc)^2 - r^2 = 0
+	// Subsituting R(t) into sphere equation
+	// (Xo + Xd*t - Xc)^2 + (Yo + Yd*t - Yc)^2 + (Zo + Zd*t - Zc)^2 - r^2 = 0
+	// rearranging we get
+	// At^2 + Bt + C = 0 where we get two solutions
+	// this is a vector from p to c
+	double a = (sqr(Rd[0]) + sqr(Rd[1]) + sqr(Rd[2]));
+	double b = ((2*Rd[0]*(Ro[0]-c[0])) + (2*Rd[1]*(Ro[1]-c[1])) + (2*Rd[2]*(Ro[2]-c[2])));
+	double d = ((sqr(Ro[0]-c[0])+sqr(Ro[1]-c[1])+sqr(Ro[2]-c[2])) - sqr(2));
+	double det = sqr(b) - 4*a*d; 
+	if( det < 0 ) return -1;
+
+	det = sqrt(det);
+
+	double t0 = (-b - det) / (2*a);
+	if (t0 > 0 ) return t0;
+
+	double t1 = (-b + det) / (2*a);
+	if(t1 > 0) return t1;
+
+	return -1;
+
+
+
+}
+void raycast(Scene scene, char* outfile, PPMmeta fileinfo){
+	PPMImage* data = malloc(sizeof(PPMImage) * fileinfo.width * fileinfo.height);
+	
+	// raycasting here
+	
+	int N = fileinfo.width;
+	int M = fileinfo.height;
+	double w = scene.camera_width;
+	double h = scene.camera_height;
+	
+	double pixel_height = h / M;
+	double pixel_width = w / N;
+	
+	double p_z = 0;
+	
+	double c_x = scene.camera_position[0];
+	double c_y = scene.camera_position[1];
+	double c_z = scene.camera_position[2];
+	
+	double r0[3];
+	r0[0] = c_x;
+	r0[1] = c_y;
+	r0[2] = c_z;
+	
+	double rd[3];
+	rd[2] = 1.0;
+	
+	int i;
+	int j;
+	int k;
+	
+	for(i = 0; i < M; i ++)
+	{
+		rd[1] = c_y + h/2.0 - pixel_height * (i + 0.5);
+		
+		for(j = 0; j < N; j ++)
+		{
+			rd[0] = c_x - w/2.0 + pixel_width * (j + 0.5);
+			
+			double best_t = INFINITY;
+			Object closest;
+
+			for(k = 0; k < scene.num_objects; k ++)
+			{
+				double t = -1;
+				Object o = scene.objects[k];
+				
+				if(o.kind == T_SPHERE)
+				{
+					double c[3];
+					c[0] = o.a;
+					c[1] = o.b;
+					c[2] = o.c;
+					t = ray_sphere_intersection(c, o.d, r0, rd);
+				}
+				else if(o.kind == T_PLANE)
+				{
+					t = ray_plane_intersection(o.a,o.b,o.c,o.d,r0,rd);
+				}
+				
+				if(t > 0 && t < best_t)
+				{
+					best_t = t;
+					closest = o;
+				}
+			}
+			
+			// write to Pixel* data
+			
+			PPMImage pixel = data[i * N + j];
+			if(best_t < INFINITY && closest.kind >= 0)
+			{
+				// makes the colors for the obejcts 
+				pixel.red = (char) (closest.color[0] * 255);
+				pixel.green = (char) (closest.color[1] * 255);
+				pixel.blue = (char) (closest.color[2] * 255);
+			}
+			else
+			{
+				// creates shadow effect
+				pixel.red = (char) (255* scene.background_color[0]);
+				pixel.green = (char) (255* scene.background_color[1]);
+				pixel.blue = (char) (255* scene.background_color[2]);
+			}
+
+			data[i * N + j] = pixel;
+			
+		}
+		
+	}
+	
+	writePPM( outfile, data, fileinfo);
+}
+
+// create my own raycasting fucntion
+int main(int argc, char** argv){
+// this is all hard code, use the parsing file for json and raycaster function
+	if(argc < 5){
+		fprintf(stderr, "Usage: width height inout.json output.ppm\n");
+		exit(1);
+	}
+	PPMmeta fileinfo;
+	fileinfo.width = atoi(argv[1]);
+	fileinfo.height = atoi(argv[2]);
+	fileinfo.max = 255;
+	fileinfo.type = 6;
+	
+	Scene scene;
+	scene = read_scene(argv[3]); 
+	// Scene scene = read_scene(argv[3]);
+
+
+	printf("Read in %d objects\n", scene.num_objects);
+
+	scene.background_color[0] = 0.5;
+	scene.background_color[1] = 0.51;
+	scene.background_color[2] = 0.6;
+
+	raycast(scene,argv[4], fileinfo);
+
+	return 0;
+}
+
